b796c41de959efe3bfd175f7efc17f9f356ef6df
Syed Burhan Ali <syed.burhan8005@gmail.com>
2023-08-14 05:48:03 +0500

    Add JS Function to check weather

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const apiKey = "17a87c3252f78a76c5882f6a5e492b8e";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=karachi";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".seach input");
+const searchBtn = document.querySelector(".search button");
 
 
-async function checkWeather() {
-    const respone = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather(city) {
+    const respone = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await respone.json();
-    
+
     console.log(data);
-    
+
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector("temp").innerHTML = data.main.temp;     
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
 
+})
